46661af97bb8eb053f042c087ff4260226d36c5f
Alexey Lukashin <alexey.lukashin@spbstu.ru>
2020-09-22 11:26:52 +0300

    Lecture 1: intro

---
+++ src/main/java/ru/spbstu/iamm/telematics/parprog/lecture1/HelloWorld.java
@@ -0,0 +1,13 @@
+package ru.spbstu.iamm.telematics.parprog.lecture1;
+
+class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello world!");
+	}
+
+	private void someMethod() {
+	
+	}
+
+}
